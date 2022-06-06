81f801d9cf141a26c16c45e9061eedf9406be670
InacioGabriel <gabriel.inacio@sptech.school>
2022-06-06 20:59:59 -0300

    site pronto

--- HTML's/src/database/config.js
+++ HTML's/src/database/config.js
@@ -21,8 +21,8 @@ var sqlServerConfig = {
 var mySqlConfig = {
     host: "localhost",
     database: "infoFUT",
-    user: "aluno",
-    password: "sptech",
+    user: "root",
+    password: "G@bi777879",
 };
 
 function executar(instrucao) {
